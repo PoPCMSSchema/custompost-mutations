364fe0d84fe3ff7fb9eb9f967b063c6dfbba69f6
Leonardo Losoviz <leo@getpop.org>
2020-11-10 16:16:27 +0800

    Renamed 'customPostID' to 'id'

--- src/MutationResolvers/MutationInputProperties.php
+++ src/MutationResolvers/MutationInputProperties.php
@@ -6,7 +6,7 @@ namespace PoPSchema\CustomPostMutations\MutationResolvers;
 
 class MutationInputProperties
 {
-    public const ID = 'customPostID';
+    public const ID = 'id';
     public const TITLE = 'title';
     public const CONTENT = 'content';
     public const STATUS = 'status';
