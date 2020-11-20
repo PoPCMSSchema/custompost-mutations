b64764976b04696215e9a0536538a5049e4a9b4a
Leonardo Losoviz <leo@getpop.org>
2020-11-20 16:12:36 +0800

    Added dependency on UserRoles

--- src/Component.php
+++ src/Component.php
@@ -20,6 +20,7 @@ class Component extends AbstractComponent
     {
         return [
             \PoPSchema\CustomPosts\Component::class,
+            \PoPSchema\UserRoles\Component::class,
             \PoPSchema\UserStateMutations\Component::class,
         ];
     }
